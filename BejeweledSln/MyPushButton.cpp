f8424dbb72f30e9e5b92696a37233a13ceb7db0a
BCAGAYAN-bcagayan <58718532+BCAGAYAN-bcagayan@users.noreply.github.com>
2020-05-28 18:36:05 +0800

    hdx new

--- BejeweledSln/MyPushButton.cpp
+++ BejeweledSln/MyPushButton.cpp
@@ -1,6 +1,50 @@
 #include "MyPushButton.h"
+#include <QDebug>
+#include <QPropertyAnimation>
+MyPushButton::MyPushButton(QString normalImg, QString pressImg){
 
-MyPushButton::MyPushButton(QWidget *parent) : QWidget(parent)
-{
+    this->normalImgPath = normalImg;
+    this->pressImgPath = pressImg;
 
+    QPixmap pix;
+    bool ret = pix.load(normalImg);
+    if(!ret)
+    {
+        qDebug()<<"加载图片失败";
+        return;
+    }
+    this->setFixedSize(pix.width(),pix.height());
+    this->setStyleSheet("QPushButton{border:0px}");//无边框
+    this->setIcon(pix);
+    this->setIconSize(QSize(pix.width(),pix.height()));
+
+
+}
+void MyPushButton:: zoom1(){
+    QPropertyAnimation * animation = new QPropertyAnimation(this,"geometry");
+    //时间间隔
+    animation->setDuration(200);
+    //起始位置
+    animation->setStartValue(QRect(this->x(),this->y(),this->width(),this->height()));
+    //结束位置
+    animation->setEndValue(QRect(this->x(),this->y()+10,this->width(),this->height()));
+    //弹跳曲线
+    animation->setEasingCurve(QEasingCurve::OutBounce);
+
+    animation->start();
+
+}
+
+void MyPushButton:: zoom2(){
+    QPropertyAnimation * animation = new QPropertyAnimation(this,"geometry");
+    //时间间隔
+    animation->setDuration(200);
+    //起始位置
+    animation->setStartValue(QRect(this->x(),this->y()+10,this->width(),this->height()));
+    //结束位置
+    animation->setEndValue(QRect(this->x(),this->y(),this->width(),this->height()));
+    //弹跳曲线
+    animation->setEasingCurve(QEasingCurve::OutBounce);
+
+    animation->start();
 }
--- BejeweledSln/MyPushButton.h
+++ BejeweledSln/MyPushButton.h
@@ -1,14 +1,21 @@
 #ifndef MYPUSHBUTTON_H
 #define MYPUSHBUTTON_H
 
-#include <QWidget>
+#include <QPushButton>
 
-class MyPushButton : public QWidget
+class MyPushButton : public QPushButton
 {
     Q_OBJECT
 public:
-    explicit MyPushButton(QWidget *parent = nullptr);
 
+    MyPushButton(QString normalImg, QString pressImg ="");
+
+    QString normalImgPath;
+    QString pressImgPath;
+
+    //按键效果
+    void zoom1();
+    void zoom2();
 signals:
 
 };
